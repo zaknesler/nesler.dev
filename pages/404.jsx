fd64fcb583e0cfe70e82d751859e56967f3f7eb4
Zak Nesler <zaknesler@gmail.com>
2020-09-04 12:47:10 -0400

    Update 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -2,8 +2,9 @@ import Layout from '../components/Layout'
 
 export default function NotFound() {
   return (
-    <Layout title="404 Not Found">
-      <div>404 Not Found</div>
+    <Layout>
+      <div className="mb-3 text-3xl font-bold text-gray-900">404</div>
+      <div>Sorry, that page could not be found!</div>
     </Layout>
   )
 }
