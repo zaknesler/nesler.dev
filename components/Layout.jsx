37fd6966bdd634cf4a12bc4454997e3c2f1aab19
Zak Nesler <zaknesler@gmail.com>
2020-09-19 17:55:13 -0400

    Add missing meta tags

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -17,9 +17,32 @@ export default class Layout extends React.Component {
     return (
       <div>
         <Head>
+          <meta charset="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta name="author" content="Zak Nesler" />
+          <meta name="robots" content="index, follow" />
+          <meta name="msapplication-starturl" content="/" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="application-name" content="Zak Nesler" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-title" content="Zak Nesler" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta
+            name="description"
+            content="Student full-stack developer from Pennsylvania with a passion for tasteful and minimal design, as well as tidy and efficient back-end code."
+          />
+          <meta
+            name="keywords"
+            content="zak nesler, zak, nesler, zaknes, zaknesler, zachary nesler, web design, design, code, html, css, php, laravel, vue, programming, web development"
+          />
+          <meta name="p:domain_verify" content="ff1f90db2750254f6843a167a5e5efb8" />.
           <title>{this.props.title ? `${this.props.title} – Zak Nesler` : 'Zak Nesler'}</title>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:400,500,700" />
           <link rel="icon" href="/favicon.png" />
+          <link rel="icon" type="image/png" sizes="128x128" href="/favicon.png" />
+          <link rel="apple-touch-icon" type="image/png" sizes="128x128" href="/favicon.png" />
+          <link rel="manifest" href="/manifest.json"></link>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" />
         </Head>
 
         <div>
