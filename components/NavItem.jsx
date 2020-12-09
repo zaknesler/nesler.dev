def8bea88c416018fd72ac7ebd15890a2c0a295c
Zak Nesler <zak@nesler.dev>
2020-12-08 23:36:15 -0500

    Highlight tool nav item when on tool subpage

--- components/NavItem.jsx
+++ components/NavItem.jsx
@@ -9,7 +9,7 @@ export default function NavItem({ href, children }) {
       <Link href={href}>
         <a
           className={
-            (router.pathname === href
+            (router.pathname === href || (router.pathname.startsWith('/tools') && href.startsWith('/tools'))
               ? 'font-bold text-gray-900 dark:text-white'
               : 'text-gray-600 dark:text-gray-400') +
             ' hover:text-black dark:hover:text-gray-100 block p-3 md:p-0 w-full hover:bg-gray-100 dark:hover:bg-gray-800 md:hover:bg-transparent rounded-lg md:rounded-none'
