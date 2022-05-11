cc2a7ec69830331a01094b802652ff4ad6787e93
Zak Nesler <zak@nesler.dev>
2022-05-11 12:15:46 -0400

    Use JSON data for resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -2,6 +2,7 @@ import { ArrowLeftIcon } from '@heroicons/react/solid'
 import { GitHubIcon, LinkedInIcon, TwitterIcon } from '../lib/icons'
 import Base from '../components/Base'
 import Link from 'next/link'
+import data from '../resume.json'
 
 const Resume = () => {
   const ListGroup = ({ items }: { items: Array<string> }) => {
@@ -35,10 +36,10 @@ const Resume = () => {
         <section className="flex flex-col items-center justify-between text-center sm:flex-row sm:text-left">
           <div>
             <div className="text-xl font-bold text-gray-900 dark:text-gray-200">
-              Zak Nesler
+              {data.name}
             </div>
             <div className="text-xs font-medium uppercase tracking-wide text-gray-600 dark:text-gray-400">
-              Full-Stack Developer
+              {data.title}
             </div>
 
             <div className="mx-auto mt-3 h-1 w-32 bg-gradient-to-r from-brand-500 to-brand-400 sm:mx-0"></div>
@@ -47,15 +48,7 @@ const Resume = () => {
           <div className="mt-4 space-y-2 font-medium sm:mt-0 sm:text-right">
             <div className="-mb-2 flex items-center justify-center sm:justify-end">
               <a
-                href="https://twitter.com/zaknesler"
-                aria-label="Twitter Profile"
-                title="Twitter Profile"
-                className="p-3 hover:opacity-75 sm:p-2"
-              >
-                <TwitterIcon />
-              </a>
-              <a
-                href="https://github.com/zaknesler"
+                href={data.urls.github}
                 aria-label="GitHub Profile"
                 title="GitHub Profile"
                 className="p-3 hover:opacity-75 sm:p-2"
@@ -63,23 +56,36 @@ const Resume = () => {
                 <GitHubIcon />
               </a>
               <a
-                href="https://linkedin.com/in/zaknesler"
+                href={data.urls.linkedin}
                 aria-label="LinkedIn Profile"
                 title="LinkedIn Profile"
                 className="p-3 hover:opacity-75 sm:p-2"
               >
                 <LinkedInIcon />
               </a>
-              <div className="ml-3 sm:ml-2">@zaknesler</div>
+              <a
+                href={data.urls.twitter}
+                aria-label="Twitter Profile"
+                title="Twitter Profile"
+                className="p-3 hover:opacity-75 sm:p-2"
+              >
+                <TwitterIcon />
+              </a>
+              <div className="ml-2 sm:ml-1">&#64;{data.username}</div>
             </div>
 
             <div>
               <a
-                href="mailto:zak@nesler.dev"
+                href={`mailto:${data.email
+                  .replace('[at]', '%40')
+                  .replace('[dot]', '%2E')}`}
                 className="hover:text-black dark:hover:text-gray-200"
-              >
-                zak@nesler.dev
-              </a>
+                dangerouslySetInnerHTML={{
+                  __html: data.email
+                    .replace('[at]', '&#64;')
+                    .replace('[dot]', '&#46;'),
+                }}
+              />
             </div>
           </div>
         </section>
@@ -93,14 +99,7 @@ const Resume = () => {
           </div>
 
           <div className="mt-4">
-            <ListGroup
-              items={[
-                'Keen eye for detail and design',
-                'Experienced with popular web technologies such as Laravel, React, and Tailwind CSS',
-                'Focused on writing self-documenting, tested (using TDD), and tidy code',
-                'Experienced with configuring LAMP/LEMP stacks from scratch',
-              ]}
-            />
+            <ListGroup items={data.qualitites} />
           </div>
         </section>
 
@@ -117,29 +116,15 @@ const Resume = () => {
             <div className="mt-1 border-b-2 border-gray-200 dark:border-gray-800"></div>
           </div>
 
-          <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-5">
-            <div className="col-span-1 -mb-3 font-medium text-black dark:text-gray-200 md:mb-0">
-              Technologies
-            </div>
-            <div className="col-span-4">
-              Laravel, Tailwind CSS, React + Next.js, Vue.js + Nuxt.js
-            </div>
-
-            <div className="col-span-1 -mb-3 font-medium text-black dark:text-gray-200 md:mb-0">
-              Languages
-            </div>
-            <div className="col-span-4">
-              PHP, HTML/CSS, TypeScript, JavaScript, SQL, Python, Node.js, Java,
-              C, C++, Go, Bash, Regex
-            </div>
-
-            <div className="col-span-1 -mb-3 font-medium text-black dark:text-gray-200 md:mb-0">
-              Software
-            </div>
-            <div className="col-span-4">
-              Docker, Git, PHPUnit, VS Code, Sublime Text, Ubuntu, Debian, LEMP,
-              LAMP, Figma, Photoshop, Blender, Excel
-            </div>
+          <div className="mt-4 space-y-4">
+            {Object.entries(data.skills).map(([name, items]) => (
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-5" key={name}>
+                <div className="col-span-1 -mb-3 font-medium text-black dark:text-gray-200 md:mb-0">
+                  {name}
+                </div>
+                <div className="col-span-4">{items.join(', ')}</div>
+              </div>
+            ))}
           </div>
         </section>
 
@@ -151,30 +136,34 @@ const Resume = () => {
             <div className="mt-1 border-b-2 border-gray-200 dark:border-gray-800"></div>
           </div>
 
-          <div className="mt-4 grid gap-4 md:grid-cols-3 lg:grid-cols-4">
-            <div className="col-span-1">
-              <div className="font-medium leading-tight text-black dark:text-gray-200">
-                Computer Visionaries, Inc.
-              </div>
-              <div className="mt-1 text-sm text-gray-500">Pittston, PA</div>
-              <div className="mt-2 flex items-center text-xs font-medium text-blue-700 dark:text-blue-200">
-                <div className="rounded-md bg-blue-50 px-2 py-1 dark:bg-blue-800 dark:bg-opacity-50">
-                  November 2021 – Present
+          <div className="mt-4 space-y-8">
+            {data.experience.map((item, idx) => (
+              <div
+                key={idx}
+                className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"
+              >
+                <div className="col-span-1">
+                  <div className="mt-1 font-medium leading-tight text-black dark:text-gray-200">
+                    {item.company}
+                  </div>
+                  <div className="mt-1 text-sm text-gray-500">
+                    {item.position}
+                  </div>
+                  <div className="mt-1 text-sm text-gray-500">
+                    {item.location}
+                  </div>
+                  <div className="mt-2 flex items-center text-xs font-medium text-blue-700 dark:text-blue-200">
+                    <div className="rounded-md bg-blue-50 px-2 py-1 dark:bg-blue-800 dark:bg-opacity-50">
+                      {item.date}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-span-2 space-y-4 lg:col-span-3">
+                  <ListGroup items={item.responsibilities} />
                 </div>
               </div>
-            </div>
-
-            <div className="col-span-2 space-y-4 lg:col-span-3">
-              <ListGroup
-                items={[
-                  'Implemented new communication logging feature covering front-end, back-end, and database with great feedback from end users',
-                  'Improved UI/UX of customer input fields',
-                  'Improved UX and loading times of main dashboard page',
-                  'Optimized SQL stored procedures',
-                  'Utilized Angular front-end, Express.js back-end, and SQL Server',
-                ]}
-              />
-            </div>
+            ))}
           </div>
         </section>
 
@@ -186,62 +175,60 @@ const Resume = () => {
             <div className="mt-1 border-b-2 border-gray-200 dark:border-gray-800"></div>
           </div>
 
-          <div className="mt-4 grid gap-4 md:grid-cols-3 lg:grid-cols-4">
-            <div className="col-span-1">
-              <div className="font-medium leading-tight text-black dark:text-gray-200">
-                Wilkes University
-              </div>
-              <div className="mt-1 text-sm text-gray-500">
-                Expected May 2022
-              </div>
-              <div className="mt-2 flex items-center text-xs font-medium text-blue-700 dark:text-blue-200">
-                <div className="rounded-md bg-blue-50 px-2 py-1 dark:bg-blue-800 dark:bg-opacity-50">
-                  GPA: 3.55
+          {data.education.map((item, idx) => (
+            <div
+              key={idx}
+              className="mt-4 grid gap-4 md:grid-cols-3 lg:grid-cols-4"
+            >
+              <div className="col-span-1">
+                <div className="font-medium leading-tight text-black dark:text-gray-200">
+                  {item.school}
                 </div>
-                <div className="ml-2 rounded-md bg-blue-50 px-2 py-1 dark:bg-blue-800 dark:bg-opacity-50">
-                  Major GPA: 3.61
+                <div className="mt-1 text-sm text-gray-500">
+                  {item.graduation}
+                </div>
+                <div className="mt-2 flex items-center text-xs font-medium text-blue-700 dark:text-blue-200">
+                  <div className="rounded-md bg-blue-50 px-2 py-1 dark:bg-blue-800 dark:bg-opacity-50">
+                    GPA: {item.gpa}
+                  </div>
+                  {item.gpa_major && (
+                    <div className="ml-2 rounded-md bg-blue-50 px-2 py-1 dark:bg-blue-800 dark:bg-opacity-50">
+                      Major GPA: {item.gpa_major}
+                    </div>
+                  )}
                 </div>
               </div>
-            </div>
 
-            <div className="col-span-2 space-y-4 lg:col-span-3">
-              <div>
-                Currently studying for a Bachelor of Science degree in Computer
-                Science.
-              </div>
+              <div className="col-span-2 space-y-4 lg:col-span-3">
+                <div>{item.description}</div>
 
-              <div>
-                <div className="font-bold text-black dark:text-gray-200">
-                  Relevant Courses & Skills
-                </div>
+                <div>
+                  <div className="font-bold text-black dark:text-gray-200">
+                    Relevant Courses &amp; Skills
+                  </div>
 
-                <div className="mt-2 grid grid-cols-1 gap-2 lg:grid-cols-2 lg:gap-4">
-                  <ListGroup
-                    items={[
-                      'Algorithms',
-                      'Data Structures',
-                      'Software Engineering',
-                      'Web Development I & II',
-                      'Computer Networks',
-                      'Concurrent & Quantum Computing',
-                      'Computer Architecture',
-                    ]}
-                  />
+                  <div className="mt-2 grid grid-cols-1 gap-2 lg:grid-cols-2 lg:gap-4">
+                    {item.courses && (
+                      <>
+                        <ListGroup
+                          items={item.courses.slice(
+                            0,
+                            item.courses.length / 2 + 1,
+                          )}
+                        />
 
-                  <ListGroup
-                    items={[
-                      'Operating Systems',
-                      'Theory of Computation',
-                      'Principles of Prog. Languages',
-                      'Compiler Design & Assembly',
-                      'Team-oriented projects',
-                      'Python, Java, C, C++, PHP, Go, Racket, Unix',
-                    ]}
-                  />
+                        <ListGroup
+                          items={item.courses.slice(
+                            item.courses.length / 2 + 1,
+                          )}
+                        />
+                      </>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </section>
       </div>
     </Base>
