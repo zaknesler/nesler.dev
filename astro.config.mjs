23e7c9dc158d059810215ad489b67aa9368d6008
Zak Nesler <zak@nesler.dev>
2024-10-08 16:03:20 -0400

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nesler.dev',
   output: 'hybrid',
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: page => !page.includes('resume'),
+      lastmod: new Date(),
+    }),
+  ],
   build: {
     assets: '_assets',
     format: 'file',
