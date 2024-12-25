e367fd4d421091815aeadac2f86c0dc14c193ffb
Zak Nesler <zak@nesler.dev>
2024-12-25 10:47:08 -0500

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nesler.dev',
-  output: 'hybrid',
+  output: 'static',
   adapter: cloudflare(),
   integrations: [
     sitemap({
