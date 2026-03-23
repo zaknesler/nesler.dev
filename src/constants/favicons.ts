e699e4c48e7956ff29ad11bd5263a9ea09cb562f
Zak Nesler <zak@nesler.dev>
2026-03-23 09:54:17 -0400

    fix

--- src/constants/favicons.ts
+++ src/constants/favicons.ts
@@ -6,5 +6,5 @@ export const favicons = [
 	{ rel: 'apple-touch-icon', type: 'image/svg', href: '/favicon.svg' },
 	{ rel: 'apple-touch-icon', type: 'image/png', sizes: '128x128', href: '/favicon-128.png' },
 	{ rel: 'apple-touch-icon', type: 'image/png', sizes: '96x96', href: '/favicon-96.png' },
-	{ rel: 'apple-touch-icon', type: 'image/png', sizes: '48x48', href: '/favicon-48.png' }
+	{ rel: 'apple-touch-icon', type: 'image/png', sizes: '48x48', href: '/favicon-48.png' },
 ];
