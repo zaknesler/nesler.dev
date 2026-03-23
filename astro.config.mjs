f75c8a4cb40388378cfd37bb0e88b9a6e4f610d9
Zak Nesler <zak@nesler.dev>
2026-03-23 09:40:03 -0400

    use oxfmt/oxlint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,22 @@
-import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nesler.dev',
-  output: 'static',
-  // adapter: cloudflare(),
-  integrations: [
-    sitemap({
-      filter: page => !page.includes('resume'),
-      lastmod: new Date(),
-    }),
-  ],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  build: {
-    assets: '_assets',
-    format: 'file',
-  },
+	site: 'https://nesler.dev',
+	output: 'static',
+	integrations: [
+		sitemap({
+			filter: page => !page.includes('resume'),
+			lastmod: new Date()
+		})
+	],
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	build: {
+		assets: '_assets',
+		format: 'file'
+	}
 });
